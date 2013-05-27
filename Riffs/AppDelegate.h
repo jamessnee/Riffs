f426cbff090acaa2aa241a685615fa84eb318a81
James Snee <james.snee@gmail.com>
2013-05-27 16:12:33 +0100

    Initial Commit

---
+++ Riffs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Riffs
+//
+//  Created by James Snee on 27/05/2013.
+//  Copyright (c) 2013 James Snee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ Riffs/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Riffs
+//
+//  Created by James Snee on 27/05/2013.
+//  Copyright (c) 2013 James Snee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
